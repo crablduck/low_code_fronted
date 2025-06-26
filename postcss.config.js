6b3fb15688d4538b4464ddebcf3419a4ddbcc0b2
KrabWW <wei17306927526@gmail.com>
2025-06-26 09:12:28 +0800

    feat: 添加 PostCSS 配置和更新依赖

---
+++ postcss.config.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: KrabWW wei17306927526@gmail.com
+ * @Date: 2025-06-26 08:54:47
+ * @LastEditors: KrabWW wei17306927526@gmail.com
+ * @LastEditTime: 2025-06-26 08:56:06
+ * @FilePath: /htsp-cloud-web/workflow-system/postcss.config.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+export default {
+  plugins: {
+    autoprefixer: {}
+  }
+} 
